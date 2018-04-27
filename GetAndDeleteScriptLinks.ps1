31a3f07b4bb3409463d65f5623dc2204f0c9550a
Kristján Ingi Einarsson <wintendo2003@gmail.com>
2018-04-27 10:21:30 +0000

    First Commit
    Script to delete Scriptlinks from site collection in SharePoint.

---
+++ GetAndDeleteScriptLinks.ps1
@@ -0,0 +1,52 @@
+﻿# replace these details if needed
+
+#$username = "username" 
+#$password = "password" 
+
+#$url = "https://tenant.sharepoint.com/sites/site/"
+
+$credentials = Get-credential
+
+$username = $credentials.UserName
+$securePassword = $credentials.Password
+ 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+$url = Read-Host -Prompt ("Enter the Url of the site")
+ 
+# connect/authenticate to SharePoint Online and get ClientContext object.. 
+$clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($url) 
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($username, $securePassword) 
+$clientContext.Credentials = $credentials 
+ 
+if (!$clientContext.ServerObjectIsNull.Value) 
+{ 
+    Write-Host "Connected to SharePoint Online site: '$Url'" -ForegroundColor Green 
+} 
+
+$site = $clientContext.Site
+$sca = $site.UserCustomActions
+
+$clientContext.Load($sca)
+
+$clientContext.ExecuteQuery() 
+
+Write-Host($sca | Format-List -Property Id, Name, Scope, Location, Sequence, ScriptSrc | Out-String)
+
+$idToDelete = Read-Host -Prompt ("Enter the ID of the scriptlink to be deleted")
+
+Foreach ($scriptLink in $sca)
+{   
+    if($scriptLink.Id -eq $idToDelete){
+        $confirm = Read-Host -Prompt ("Are you sure you want to delete " + $scriptLink.Id + " (y/n)")
+
+        if($confirm -eq "y"){
+            Write-Host("Deleting " + $scriptLink.Id)
+            $scriptLink.DeleteObject()
+            $clientContext.ExecuteQuery() 
+        }
+    }
+}
+
+
